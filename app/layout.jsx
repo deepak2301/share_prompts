f6d37e0fa16320665b1979cd5cd9e2abf7e94416
lxydeep1 <entrepreneur.deepak2023@gmail.com>
2024-10-20 23:33:39 +0530

    setting up nextAuth

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,14 +12,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
 
-          {children}
-        </main>
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );

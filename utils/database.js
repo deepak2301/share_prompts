6b86acd07f90f76c68ba3eecfbd36193a3697afa
lxydeep1 <entrepreneur.deepak2023@gmail.com>
2024-10-20 23:43:38 +0530

    adding next auth

---
+++ utils/database.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+let isConnected = false; // track the connection
+
+export const connection = async () => {
+    mongoose.set('strictQuery',true)
+
+    if(isConnected){
+        console.log('MongoDB is already Connected!');
+        return
+        
+    }
+    try {
+       await mongoose.connect(process.env.MONGODB_URI,{
+        dbName:"share_prompt",
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+       })
+       isConnected=true
+       console.log("MongoDb Connected");
+       
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
+}
